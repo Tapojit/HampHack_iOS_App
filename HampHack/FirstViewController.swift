7fa9b559abbc21c6164f40f30f31a2441d8118df
Ziyu Wang <selinawang@hampguest78-243.hampshire.edu>
2017-03-03 19:28:33 -0500

    Initial Commit

---
+++ HampHack/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HampHack
+//
+//  Created by Ziyu Wang on 3/3/17.
+//  Copyright © 2017 HampHack_Tapojit. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
